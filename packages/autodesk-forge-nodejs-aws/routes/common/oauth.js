c01b0ca27464e00af4076f86cffee6a653b48d6e
Vinod Shukla <vsnyc@users.noreply.github.com>
2018-09-21 03:09:49 -0400

    Adding source directory

---
+++ packages/autodesk-forge-nodejs-aws/routes/common/oauth.js
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+const { AuthClientTwoLegged } = require('forge-apis');
+
+const configAWS = require('../../configAWS');
+
+/**
+ * Initializes a Forge client for 2-legged authentication.
+ * @param {string[]} scopes List of resource access scopes.
+ * @returns {AuthClientTwoLegged} 2-legged authentication client.
+ */
+async function getClient(scopes) {
+    const client_id = await configAWS.forgeAWSClientId();
+    const client_secret = await configAWS.forgeAWSClientSecret();
+    return new AuthClientTwoLegged(client_id, client_secret, scopes || configAWS.scopeInternal);
+}
+
+let cache = {};
+async function getToken(scopes) {
+    const key = scopes.join('+');
+    if (cache[key]) {
+        return cache[key];
+    }
+    const client = await getClient(scopes);
+    let credentials = await client.authenticate();
+    cache[key] = credentials;
+    setTimeout(() => { delete cache[key]; }, credentials.expires_in * 1000);
+    return credentials;
+}
+
+/**
+ * Retrieves a 2-legged authentication token for preconfigured public scopes.
+ * @returns Token object: { "access_token": "...", "expires_at": "...", "expires_in": "...", "token_type": "..." }.
+ */
+async function getPublicToken() {
+    return getToken(configAWS.scopePublic);
+}
+
+/**
+ * Retrieves a 2-legged authentication token for preconfigured internal scopes.
+ * @returns Token object: { "access_token": "...", "expires_at": "...", "expires_in": "...", "token_type": "..." }.
+ */
+async function getInternalToken() {
+    return getToken(configAWS.scopeInternal);
+}
+
+module.exports = {
+    getClient,
+    getPublicToken,
+    getInternalToken
+};
